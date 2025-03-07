36a25340cb94f69f262d5fcc1beb95fd7561f95c
Aquilavibes <aquilabrama312@gmail.com>
2025-03-07 18:24:07 +0100

    first commit to mac

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="min-h-screen bg-gradient-to-r from-[#001F3F] via-[#0B0C10] to-[#4B0082]">
+<NuxtPage />
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<header class="text-white ml-[90px] text-[25px]"> $Ether-Check</header>
+
+    <div class="justify-center flex mt-[50px] gap-5">
+<h1 class="text-[50px] text-white mr-[400px] absolute font-mono">Check how broke or rich<br> someone is with just the<br> ETH Address</h1>
+
+<img src="/eth.png" alt='ethereum image' class="h-[850px] w-[8500px] relative ml-[250px] -mt-[100px]" />
+
+    </div>
+<small class="text-white/80 text-[15px] absolute mt-[300px] top-10 left-[200px] "> start having fun by monitoring people's address </small>
+<button class="text-white bg-gradient-to-r from-[#001F3F] via-[#0B0C10] to-[#4B0082] w-[150px] h-[50px] absolute mt-[400px] top-10 left-[200px]">
+    Start Checking</button>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
