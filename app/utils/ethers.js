2a1f7a89d2e6b16ae50c669d00e5382f6a756b82
Cyber Quils <105954716+Aquilavibes@users.noreply.github.com>
2025-03-22 05:23:34 -0700

    fixed rpc error

--- app/utils/ethers.js
+++ app/utils/ethers.js
@@ -6,7 +6,7 @@ export async function getEthBalance(address) {
       throw new Error("Invalid Ethereum address");
     }
 
-    const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth"); // Use a free RPC provider
+    const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth/b0b551a8b2c5b7236e99982a57ae1b7bef7991a0b062a1dbe005cd37bbe7856e"); // Use a free RPC provider
     const balance = await provider.getBalance(address);
     
     return ethers.formatEther(balance); // Convert balance from Wei to ETH
